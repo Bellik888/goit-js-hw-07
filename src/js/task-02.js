f75063fe419e8cbba1edb876411afa5ed7fa69fa
Bellik888 <Bellik888@gmail.com>
2021-07-17 18:10:04 +0300

    task_02 completed

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,3 +1,9 @@
+// Напиши скрипт, який для кожного елемента масиву ingredients створить окремий li,
+// після чого вставить всі li за одну операцію в список ul.ingredients.
+// Для створення DOM - вузлів використовуй document.createElement().
+
+
+
 const ingredients = [
   'Картошка',
   'Грибы',
@@ -6,3 +12,13 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+const ingredientsRef = document.querySelector('#ingredients');
+
+ingredients.forEach(ingredient => {
+  const item = document.createElement('li');
+  item.textContent = ingredient;
+  ingredientsRef.appendChild(item);
+});
+
+console.log(ingredientsRef);
