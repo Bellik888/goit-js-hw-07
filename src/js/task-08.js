99a5242f4d2c19e94907a28d7b3dad660f8c0113
Bellik888 <Bellik888@gmail.com>
2021-07-19 18:55:32 +0300

    Revert "task-08 add different size for div"
    This reverts commit cc943f18eaa252a0376eeaa928f4498b6263ff0b.

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -34,17 +34,12 @@ function createBoxes(amount) {
    for (let i = 0; i < `${amount}`; i++) {
        arr.push(i);
    }
-    let startWidth = 20;
-    let startHeight = 20;
     arr.map(el => {
         const div = document.createElement('div');
-        div.setAttribute('style', 'display: block; margin: 20px auto;  background-color: rgb(220,20,60); ');
-        div.style.width = (startWidth += 10) + 'px';
-        div.style.height = (startHeight += 10) + 'px';
+        div.setAttribute('style', 'display: block; margin: 20px auto;  background-color: red; width: 30px; height: 30px');
         refs.wrapperDiv.append(div);
     })
 };
-
 
 function onDestroyCollection() {
     refs.wrapperDiv.innerHTML = '';
